d7d9c0cd66b8c855c4edb7f7c597c58a376c3520
ClancyFisher <clancy.fisher@stu.bryanisd.org>
2015-12-03 10:47:28 -0600

    Added weapon class

---
+++ WeaponInterface.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.awt.event.*;
+public class WeaponInterface{
+    
+    public WeaponInterface(){
+       
+    }
+       
+    public void DragonWithAids(MouseEvent mouse)
+    {
+        System.out.println(mouse.getPoint());
+        
+    }   
+   
+}
