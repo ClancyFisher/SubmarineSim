911fb143ff4113f484725643f389d2388c9d21fe
ClancyFisher <cf34081@BHS-88169-5151S.bisd.org>
2015-12-07 10:21:12 -0600

    still broke

--- InterMcFace.java
+++ InterMcFace.java
@@ -5,49 +5,67 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 
-public class InterMcFace extends JPanel implements ActionListener, MouseListener, MouseMotionListener
+public class InterMcFace extends JPanel implements ActionListener, MouseListener, MouseMotionListener, KeyListener
 {   
-
+    
     WeaponInterface Wep = new WeaponInterface();
     Ship shp = new Ship(100,100);
     Radar radar = new Radar(shp,659,437);
     BufferedImage img = null;
+    
+    
+    boolean radarClick = false;
     public InterMcFace()
     {
         setPreferredSize(new Dimension(1920,1080));
-        
+
         setBackground(Color.BLUE.darker());
-        
+
         JFrame frame = new JFrame("InterMcFramealoo");
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         frame.setUndecorated(true);
         frame.add(this);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.addKeyListener(this);
         frame.addMouseListener(this);
         frame.addMouseMotionListener(this);
-       
+
         try {
             img = ImageIO.read(new File("InterfaceLayout.png"));
         } catch (Exception e) {
         }
+
         
         Timer t = new Timer(5, this);
         t.start();
     }
-    
+
     public void actionPerformed(ActionEvent ae)
     {
         repaint();
     }
-    
+
     public void mouseClicked(MouseEvent mouse) 
     {
-        
+
     }
 
-    public void mousePressed(MouseEvent mouse) {}
+    public void mousePressed(MouseEvent mouse) {
+        Point m = mouse.getPoint();
+        if(radar.getBounds().contains(m))
+        {
+            radarClick = true;    
+            
+        }
+        
+        if(radarClick) {
+            radar.click(m.x,m.y);
+            System.out.println(m.getX() + ", " + m.getY());
+            System.out.println("Clicked inside the radar");
+        }
+    }
 
     public void mouseReleased(MouseEvent mouse) 
     {
@@ -55,8 +73,11 @@ public class InterMcFace extends JPanel implements ActionListener, MouseListener
         System.out.println(m.getX() + ", " + m.getY());
         if(radar.getBounds().contains(m))
         {
-            radar.click(m.x,m.y);
+            radarClick = false;    
+            
         }
+        
+        
         repaint();
     }
 
@@ -65,11 +86,24 @@ public class InterMcFace extends JPanel implements ActionListener, MouseListener
     public void mouseExited(MouseEvent mouse) {}
 
     public void mouseDragged(MouseEvent mouse) {}
-    
+
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         draw(g);
+    }
+
+    public void keyTyped(KeyEvent input) {
+        char key = input.getKeyChar();
+        if (key == 27) System.exit(0);
+    }
+
+    public void keyPressed(KeyEvent txt) {
+        //do nothing
+    }
+
+    public void keyReleased(KeyEvent txt) {
+        //do nothing
     }
     
     public void draw(Graphics g)
@@ -79,14 +113,14 @@ public class InterMcFace extends JPanel implements ActionListener, MouseListener
         //g.setColor(Color.GREEN.darker());
         //g.drawRect(659, 437, 600, 600);
         //g.drawImage(Wep.getImage(), 20, 350, null);
-        
+
     }
-    
+
     public void mouseMoved(MouseEvent mouse)
     {
-       
+
     }
-    
+
     public static void main(String[] args)
     {
         new InterMcFace();
